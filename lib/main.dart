137b402a64ef753a08adf135a4781351f2ce7ed9
Chathra Navoda <chathra96@gmail.com>
2022-12-30 19:42:20 +0530

    configure firebase firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:educational_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
