a6248945a99451656027f135b17b3740fcb1bd36
Chathra Navoda <chathra96@gmail.com>
2023-01-08 17:06:33 +0530

    singleton class for fontFam

---
+++ lib/configs/themes/app_icons.dart
@@ -0,0 +1,8 @@
+//singleton
+import 'package:flutter/cupertino.dart';
+
+class AppIcons {
+  AppIcons._();
+  static const fontFam = 'AppIcons';
+  static const IconData trophyOutLine = IconData(0xe808, fontFamily: fontFam);
+}
