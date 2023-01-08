c757c277f674abb903e59b1a9319496a29ae9548
Chathra Navoda <chathra96@gmail.com>
2023-01-08 14:36:50 +0530

    set up variables in ui_parmeters class

---
+++ lib/screens/home/question_card.dart
@@ -0,0 +1,72 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:educational_app/configs/themes/custom_text_style.dart';
+import 'package:educational_app/configs/themes/ui_parameters.dart';
+import 'package:educational_app/models/question_paper_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+class QuestionCard extends StatelessWidget {
+  const QuestionCard({super.key, required this.model});
+
+  final QuestionPaperModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    const double _padding = 10.0;
+    return Padding(
+      padding: const EdgeInsets.all(_padding),
+      child: Stack(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: ColoredBox(
+                  color: Theme.of(context).primaryColor.withOpacity(0.2),
+                  child: SizedBox(
+                    height: Get.width * 0.15,
+                    width: Get.width * 0.15,
+                    child: CachedNetworkImage(
+                      imageUrl: model.imageUrl!,
+                      placeholder: (context, url) => Container(
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          Image.asset("assets/images/app_splash_logo.png"),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      model.title,
+                      style: cardTitles(context),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 15),
+                      child: Text(model.description),
+                    ),
+                    Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
