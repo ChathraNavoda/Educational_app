9d6757979aa15c14de9f27670ea0e8475337e40b
Chathra Navoda <chathra96@gmail.com>
2023-01-11 21:46:41 +0530

    styling login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -13,7 +13,30 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(gradient: mainGradient()),
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: mainGradient(),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              width: 200,
+              height: 200,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 60),
+              child: Text(
+                "This is a study app. You can use it as you want. If you understand how this works, you would be able to scale it.",
+                style: TextStyle(
+                    color: onSurfaceTextColor, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
